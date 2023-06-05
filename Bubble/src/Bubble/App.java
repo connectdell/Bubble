ef1dc359fad5183582e663477c402a1f3e56acd5
connectdell <1511297735@qq.com>
2023-06-05 13:57:32 +0800

    第二次提交代码,优化了3个java文件

--- Bubble/src/Bubble/App.java
+++ Bubble/src/Bubble/App.java
@@ -1,72 +1,83 @@
 package Bubble;
-import java.awt.Graphics;
+
+import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
-import java.util.Timer;
-import java.util.TimerTask;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+
 public class App extends JFrame {
     private static final long serialVersionUID = -4664152594554232786L;
-    private int interval = 1000/100;
+    private int interval = 1000 / 100;
     private Panel panel;
+
     public App() {
         panel = new Panel();
         add(panel);
 
-        setSize(800,620);
+        setTitle("Game Title");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
     }
+
     public void addKeyListener(KeyListener l) {
         panel.addKeyListener(l);
-        panel.setFocusable(true);
-        panel.setRequestFocusEnabled(true);
+        panel.requestFocusInWindow();
     }
-    public void addMouseLintener(MouseAdapter l) {
+
+    public void addMouseListener(MouseAdapter l) {
         panel.addMouseListener(l);
         panel.addMouseMotionListener(l);
         panel.addMouseWheelListener(l);
+        panel.requestFocusInWindow();
     }
-    public App(String title) {
-        this();
-        setTitle(title);
-    }
-    public void setSize(int width, int height) {
-        super.setSize(width, height);
-        setLocationRelativeTo(null);
-    }
+
     public void setInterval(int interval) {
         this.interval = interval;
     }
+
     public void start() {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        Timer timer = new Timer(interval, new ActionListener() {
             @Override
-            public void run() {
+            public void actionPerformed(ActionEvent e) {
                 running();
                 panel.repaint();
             }
-        }, interval, interval);
+        });
+        timer.start();
         setVisible(true);
     }
+
     /**
      * 重写此方法会自动执行
      */
     public void running() {
     }
+
     /**
      * 重写此方法，绘制自定义组件
      */
     public void painting(Graphics2D g) {
     }
-    private class Panel extends JPanel{
+
+    private class Panel extends JPanel {
         private static final long serialVersionUID = 8365407815677076591L;
 
         @Override
-        public void paint(Graphics g) {
-            super.paint(g);
-            painting((Graphics2D)g);
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            painting((Graphics2D) g);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(800, 620);
         }
     }
 }
--- Bubble/src/Bubble/Ball.java
+++ Bubble/src/Bubble/Ball.java
@@ -1,51 +1,55 @@
 package Bubble;
+
 import java.awt.Color;
 import java.awt.Graphics2D;
+
 public class Ball {
-    int d;
-    double x;
-    double y;
-    int i;
-    int j;
-    int k;
-    Color color;
-    double offsetX;
-    double offsetY;
-    int width;  // 增加宽度参数
-    int height; // 增加高度参数
-    // 赋值
+    private int d;
+    private double x;
+    private double y;
+    private int i;
+    private int j;
+    private int k;
+    private Color color;
+    private double offsetX;
+    private double offsetY;
+    private int width;
+    private int height;
+
     public Ball(int width, int height) {
-        this.width = width;   // 初始化宽度参数值
-        this.height = height; // 初始化高度参数值
-        d = (int)(Math.random() * 50) + 10;
-        x = (int)(Math.random() * (width - d));
-        y = (int)(Math.random() * (height - d));
-        i = (int)(Math.random() * 256);
-        j = (int)(Math.random() * 256);
-        k = (int)(Math.random() * 256);
-        color= new Color(i, j, k);
-        offsetX = Math.random() * 5 + 1;  // x坐标随机变量
-        offsetY = Math.random() * 5 + 1;  // y坐标随机变量
+        this.width = width;
+        this.height = height;
+        initialize();
+    }
+
+    private void initialize() {
+        d = (int) (Math.random() * 50) + 10;
+        x = (int) (Math.random() * (width - d));
+        y = (int) (Math.random() * (height - d));
+        i = (int) (Math.random() * 256);
+        j = (int) (Math.random() * 256);
+        k = (int) (Math.random() * 256);
+        color = new Color(i, j, k);
+        offsetX = Math.random() * 5 + 1;
+        offsetY = Math.random() * 5 + 1;
         offsetX = Math.random() < 0.5 ? -offsetX : offsetX;
         offsetY = Math.random() < 0.5 ? -offsetY : offsetY;
     }
+
     public void move() {
         x += offsetX;
         y += offsetY;
 
-        if (x <= 0 || x + d >= width) {  // 使用成员变量代替getWidth()方法
+        if (x <= 0 || x + d >= width) {
             offsetX = -offsetX;
         }
-        if (y <= 0 || y + d >= height) {  // 使用成员变量代替getHeight()方法
+        if (y <= 0 || y + d >= height) {
             offsetY = -offsetY;
         }
     }
-    public void paint1(Graphics2D g) {
-        g.setColor(color);
-        g.fillOval((int)x, (int)y, (int)d, (int)d);
-    }
+
     public void paint(Graphics2D g) {
         g.setColor(color);
-        g.fillOval((int)x, (int)y, (int)d,(int)d);
+        g.fillOval((int) x, (int) y, d, d);
     }
 }
